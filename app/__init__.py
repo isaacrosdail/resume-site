f3e5ab1952629252cd656aa68fd021d2873ffd07
Isaac-rosdail <idrfritcher@gmail.com>
2025-02-06 14:10:03 +0000

    Add templates/index.html and changed home function return accordingly

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create instance of a Flask web app
 app = Flask(__name__)
@@ -6,7 +6,12 @@ app = Flask(__name__)
 # Homepage
 @app.route("/")
 def home():
-    return "<p>Hello! this is the main page!</p>"
+    return render_template("index.html")
+
+# Contact page
+@app.route("/contact")
+def contact():
+    return "<p>Hello! Welcome to the Contact page!</p>"
     
 if __name__ == "__main__":
     app.run()
--- dev-notes/setup.md
+++ dev-notes/setup.md
@@ -2,7 +2,10 @@
 
 1. Activating venv for project
 - Needed to keep dependencies/versions used by project independent from other projects, for clarity & consistency
-- Windows (PWS): source venv/Scripts/activate
+- Windows (PWS): ```source venv/Scripts/activate```
 
 2. Remember to deactivate venv when done
 - Windows (PWS): deactivate
+
+3. Running from PowerShell (inside venv):
+- ```python "app/__init__.py"```
