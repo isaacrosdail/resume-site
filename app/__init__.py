782e01e2d472188ed6d279466a34286d4b516054
Isaac-rosdail <idrfritcher@gmail.com>
2025-05-24 21:02:49 +0100

    Tweak BP names

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 
 # Import Blueprints for routes
-from .main.routes import main
-from .auth.routes import auth
+from app.core.routes import main_bp
+from app.auth.routes import auth_bp
 
 def create_app():
+
     app = Flask(__name__) # Create instance of a Flask web app
+
     app.secret_key = "secret-key"
-    app.register_blueprint(main)
-    app.register_blueprint(auth)
+
+    # Register Route Blueprints
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+
     return app
