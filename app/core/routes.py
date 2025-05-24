943bea6ade13df1225b6fca401a8f32d442fa551
Isaac-rosdail <idrfritcher@gmail.com>
2025-05-24 21:08:44 +0100

    Copy over navbar, adjust formatting for navbar, remove Bootstrap, minor tweaks

--- app/core/routes.py
+++ app/core/routes.py
@@ -5,16 +5,16 @@ from flask import Blueprint, render_template, request
 from app.strings_en import STRINGS as EN
 from app.strings_de import STRINGS as DE
 
-main = Blueprint("main", __name__)
+main_bp = Blueprint("main", __name__, template_folder="templates")
 
 # Main homepage route
-@main.route("/")
+@main_bp.route("/", methods=["GET"])
 def home():
     lang = request.cookies.get("lang", "en")
     strings = DE if lang == "de" else EN
     return render_template("index.html", strings=strings["index"], lang=lang)
 
 # Contact page
-@main.route("/contact")
+@main_bp.route("/contact")
 def contact():
     return "<p>Hello! Welcome to the Contact page!</p>"
